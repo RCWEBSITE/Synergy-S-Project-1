418f41154e4ad6e624d3098d6a54ec5f640c40ed
RCWEBSITE <112691984+RCWEBSITE@users.noreply.github.com>
2022-09-02 11:06:46 +0200

    Add files via upload

---
+++ site internet 2022/immersion.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", function (){
+    var navbar = document.getElementById("navbar")
+    navbar.classList.toggle("sticky", window.scrollY > 0)
+})
+
+
+function addbtn(){
+
+
+var btn = document.querySelectorAll("inscription")
+btn.
+
+
+}
+
+
+
